9b4c6b7ca91570b0443de0885e5e199a9e6ea555
Askar <askarikus@yandex.ru>
2024-08-12 21:43:08 +0200

    init

---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>Register</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'McvRegister',
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submitted form')
+      this.$store
+        .dispatch(actionTypes.register, {
+          email: this.email,
+          username: this.username,
+          password: this.password,
+        })
+        .then((user) => {
+          console.log('successfully registered user', user)
+          this.$router.push({name: 'globalFeed'})
+        })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>Register</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'McvRegister',
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submitted form')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
